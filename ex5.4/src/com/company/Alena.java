ebebc7f09178747a978cdaf19823286cf0faf926
Kragel <61646979+Kragel@users.noreply.github.com>
2020-03-09 15:35:54 +0300

    Add files via upload

---
+++ ex5.4/src/com/company/Alena.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Alena {
+
+    Alena()
+    {
+        System.out.println("Alena privet");
+    }
+    Alena(String s)
+    {
+        System.out.println(s);
+    }
+}
---
+++ ex5.4/src/com/company/Main.java
@@ -0,0 +1,11 @@
+/* Add an overloaded constructor to the previous exercise that takes a
+String argument and prints it along with your message. */
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Alena a = new Alena();
+        Alena a2 = new Alena("Nu privet");
+    }
+}
